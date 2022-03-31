59f0f1b1568c3acb3521e410c06b8d59286b039a
sunqianhu <469661426@qq.com>
2022-03-31 18:23:29 +0800

    跟进

--- www/config.php
+++ www/config.php
@@ -4,9 +4,20 @@
 */
 
 return array(
-    'site_name'=>'管理系统',
-    'site_domain'=>'http://manage.sunqianhu123.cc/',
-    'view_path'=>__DIR__.'/view/',
-
+    'site_name'=>'管理系统', // 站点名称
+    'site_domain'=>'http://manage.sunqianhu123.cc/', // 站点访问域名
+    'site_path'=>__DIR__.'/', // 站点根目录
+    'view_path'=>__DIR__.'/view/', // 视图根目录
+    
+    // 数据库
+    'db'=>array(
+        'type'=>'mysql',
+        'host'=>'127.0.0.1',
+        'port'=>'3306',
+        'database'=>'yydizi',
+        'username'=>'yydizi',
+        'password'=>'YawQD0ksYlY8s9AS',
+        'charset'=>'utf8mb4'
+    )
 );
 ?>
