50e27c82065ab3c05bec6ff86c31c800e16320f6
sunqianhu <469661426@qq.com>
2022-06-14 17:46:50 +0800

    修改头像增加ie浏览器的支持

--- www/js/user_home/edit_head.js
+++ www/js/user_home/edit_head.js
@@ -97,6 +97,25 @@ editHead.submit = function(){
     }, "image/png");
 }
 
+if (!HTMLCanvasElement.prototype.toBlob) {
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+        value: function(callback, type, quality) {
+            var canvas = this;
+            setTimeout(function() {
+                var binStr = atob(canvas.toDataURL(type, quality).split(',')[1]);
+                var len = binStr.length;
+                var arr = new Uint8Array(len);
+                for (var i = 0; i < len; i++) {
+                    arr[i] = binStr.charCodeAt(i);
+                }
+                callback(new Blob([arr], {
+                    type: type || 'image/png'
+                }));
+            });
+        }
+    });
+}
+
 $(function(){
     editHead.cropperInit();
 });
